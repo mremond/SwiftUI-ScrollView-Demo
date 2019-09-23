a3aa17849d463899e5119b18f27b2ebfad504c8f
Mickael Remond <mremond@process-one.net>
2019-09-23 12:45:46 +0200

    Add empty project

---
+++ SwiftUI-ScrollView-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-ScrollView-Demo
+//
+//  Created by Mickaël Rémond on 23/09/2019.
+//  Copyright © 2019 ProcessOne. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
