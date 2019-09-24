11fa59d94403c733e600b40420a4461f8c6e955b
Mickael Remond <mremond@process-one.net>
2019-09-24 18:16:18 +0200

    Our custom scroll view can now be scroll with standard iOS drag gesture.

--- SwiftUI-ScrollView-Demo/ReverseScrollView.swift
+++ SwiftUI-ScrollView-Demo/ReverseScrollView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct ReverseScrollView<Content>: View where Content: View {
     @State private var contentHeight: CGFloat = CGFloat.zero
+    @State private var scrollOffset: CGFloat = CGFloat.zero
+    @State private var currentOffset: CGFloat = CGFloat.zero
     
     var content: () -> Content
     
@@ -17,7 +19,8 @@ struct ReverseScrollView<Content>: View where Content: View {
     func offset(outerheight: CGFloat, innerheight: CGFloat) -> CGFloat {
         print("outerheight: \(outerheight) innerheight: \(innerheight)")
         
-        return -(innerheight/2 - outerheight/2)
+        let totalOffset = currentOffset + scrollOffset
+        return -((innerheight/2 - outerheight/2) - totalOffset)
     }
     
     var body: some View {
@@ -30,7 +33,45 @@ struct ReverseScrollView<Content>: View where Content: View {
             .frame(height: outerGeometry.size.height)
             .offset(y: self.offset(outerheight: outerGeometry.size.height, innerheight: self.contentHeight))
             .clipped()
+            .animation(.easeInOut)
+            .gesture(
+                 DragGesture()
+                    .onChanged({ self.onDragChanged($0) })
+                    .onEnded({ self.onDragEnded($0, outerHeight: outerGeometry.size.height)}))
         }
+    }
+    
+    func onDragChanged(_ value: DragGesture.Value) {
+        // Update rendered offset
+        print("Start: \(value.startLocation.y)")
+        print("Start: \(value.location.y)")
+        self.scrollOffset = (value.location.y - value.startLocation.y)
+        print("Scrolloffset: \(self.scrollOffset)")
+    }
+    
+    func onDragEnded(_ value: DragGesture.Value, outerHeight: CGFloat) {
+        // Update view to target position based on drag position
+        let scrollOffset = value.location.y - value.startLocation.y
+        print("Ended currentOffset=\(self.currentOffset) scrollOffset=\(scrollOffset)")
+        
+        let topLimit = self.contentHeight - outerHeight
+        print("toplimit: \(topLimit)")
+        
+        // Negative topLimit => Content is smaller than screen size. We reset the scroll position on drag end:
+        if topLimit < 0 {
+             self.currentOffset = 0
+        } else {
+            // We cannot pass bottom limit (negative scroll)
+            if self.currentOffset + scrollOffset < 0 {
+                self.currentOffset = 0
+            } else if self.currentOffset + scrollOffset > topLimit {
+                self.currentOffset = topLimit
+            } else {
+                self.currentOffset += scrollOffset
+            }
+        }
+        print("new currentOffset=\(self.currentOffset)")
+        self.scrollOffset = 0
     }
 }
 
