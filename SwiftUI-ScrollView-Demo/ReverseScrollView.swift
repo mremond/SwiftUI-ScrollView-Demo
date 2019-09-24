fe1ecf9cf397f4438dc7d128a701bb361330f827
Mickael Remond <mremond@process-one.net>
2019-09-24 17:45:02 +0200

    Read content height info to align content to bottom of the scroll view

--- SwiftUI-ScrollView-Demo/ReverseScrollView.swift
+++ SwiftUI-ScrollView-Demo/ReverseScrollView.swift
@@ -9,23 +9,54 @@
 import SwiftUI
 
 struct ReverseScrollView<Content>: View where Content: View {
+    @State private var contentHeight: CGFloat = CGFloat.zero
+    
     var content: () -> Content
+    
+    // Calculate content offset
+    func offset(outerheight: CGFloat, innerheight: CGFloat) -> CGFloat {
+        print("outerheight: \(outerheight) innerheight: \(innerheight)")
+        
+        return -(innerheight/2 - outerheight/2)
+    }
     
     var body: some View {
         GeometryReader { outerGeometry in
             // Render the content
             //  ... and set its sizing inside the parent
             self.content()
+            .modifier(ViewHeightKey())
+            .onPreferenceChange(ViewHeightKey.self) { self.contentHeight = $0 }
             .frame(height: outerGeometry.size.height)
+            .offset(y: self.offset(outerheight: outerGeometry.size.height, innerheight: self.contentHeight))
             .clipped()
         }
+    }
+}
+
+struct ViewHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = value + nextValue()
+    }
+}
+
+extension ViewHeightKey: ViewModifier {
+    func body(content: Content) -> some View {
+        return content.background(GeometryReader { proxy in
+            Color.clear.preference(key: Self.self, value: proxy.size.height)
+        })
     }
 }
 
 struct ReverseScrollView_Previews: PreviewProvider {
     static var previews: some View {
         ReverseScrollView {
-            BubbleView(message: "Hello")
+            VStack {
+                ForEach(demoConversation.messages) { message in
+                    BubbleView(message: message.body)
+                }
+            }
         }
         .previewLayout(.sizeThatFits)
     }
